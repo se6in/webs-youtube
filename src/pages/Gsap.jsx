e1f88b3fe5b3f9312f9162e1df651506fcdfb9e6
sebin <sebin2752@naver.com>
2024-08-29 17:42:19 +0900

    페이지 만들기

---
+++ src/pages/Gsap.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Gsap() {
+  return (
+    <div>
+      Gsap
+    </div>
+  )
+}
+
+export default Gsap
---
+++ src/pages/Port.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Port() {
+  return (
+    <div>
+      Port
+    </div>
+  )
+}
+
+export default Port
